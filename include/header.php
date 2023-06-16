7c6dc28e574ba458ff7172e815d26bfcd8247366
Sumit Kumar <sumit.cotginanalytics@gmail.com>
2023-06-16 18:40:22 +0530

    two section

---
+++ include/header.php
@@ -0,0 +1,16 @@
+<nav class="navbar py-4">
+  <div class="container custom-container justify-content-lg-between">
+    <a href="javascript:void(0)" class="nav-links main-menu">MENU</a>
+    <a class="navbar-brand" href="/">
+        <img src="images/logo.png" alt="Logo" title="Logo" class="img-fluid" />
+    </a>
+    <a href="javascript:void(0)" class="nav-links d-lg-block d-none">
+        <div class="d-inline-block menu-bar">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+        OUR PRODUCTS
+    </a>
+  </div>
+</nav>
--- index.php
+++ index.php
@@ -1,13 +1,39 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Manarti Exports</title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-    
-    <script src="js/custom.js"></script>
-</body>
-</html>
+<?php include('include/head.php'); ?>
+<?php include('include/header.php'); ?>
+
+<!-- hero section -->
+<section class="hero-section h-100-vh py-5">
+    <div class="container custom-container position-relative">
+        <div class="row align-items-center">
+            <div class="col-lg-7">
+                <h1 class="main-title text-lg-start text-center">Revolutionizing Medical QUALITY PRODUCTS -Experience the <span class="text-gradient">MANARTI EXPORTS</span></h1>
+            </div>
+            <div class="col-lg-5 text-lg-start text-center">
+                <img src="images/banner.png" alt="Revolutionizing Medical QUALITY PRODUCTS -Experience the MANARTI EXPORTS" class="img-fluid" />
+            </div>
+        </div>
+        <div class="scroll-text">
+            <span class="text-white">Scroll Down</span>
+            <svg width="24" height="34" viewBox="0 0 24 34" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M2 12C2 9.34783 3.05357 6.8043 4.92893 4.92893C6.8043 3.05357 9.34783 2 12 2C14.6522 2 17.1957 3.05357 19.0711 4.92893C20.9464 6.8043 22 9.34783 22 12V22C22 24.6522 20.9464 27.1957 19.0711 29.0711C17.1957 30.9464 14.6522 32 12 32C9.34783 32 6.8043 30.9464 4.92893 29.0711C3.05357 27.1957 2 24.6522 2 22V12Z" stroke="white" stroke-width="2.5" stroke-linejoin="round"/>
+                <path d="M12 8.66669V15.3334" stroke="white" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+        </div>
+    </div>
+</section>
+<!-- hero section end -->
+
+<!-- about section -->
+<section class="h-100-vh py-5">
+    <div class="container custom-container">
+        <div class="row align-items-center">
+            <div class="col-lg-6 mx-auto">
+                <p class="about-text mb-0">Manarti Exports is a specialized medical disposables, systems, devices and hospital equipment provider offering a wide range of products. Our products are manufactured at state-of-the art production facilities in India, with capacity to produce large quantities. We are ISO 9001:2008 and ISO 13485:2003 certified and our products are CE Marked. Our Medical Devices and Catheters are FDA Approved.</p>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- about section end -->
+
+
+<?php include('include/footer.php'); ?>
