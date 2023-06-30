e7f7725688dc6a9a4be5beea127587364ea2a8d2
Sumit Kumar <sumit.cotginanalytics@gmail.com>
2023-06-30 18:43:19 +0530

    chatbot

--- include/footer.php
+++ include/footer.php
@@ -104,6 +104,20 @@
 <script src="js/fancybox.min.js"></script>
 <script src="js/custom.js?v=<?=time();?>"></script>
 
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/649ed46894cf5d49dc60bd2b/1h466rerp';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+
 </body>
 
 </html>
