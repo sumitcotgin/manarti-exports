592dc229a80715c226ee9d61fb7d8a050519747c
Sumit Kumar <sumit.cotginanalytics@gmail.com>
2023-07-05 13:58:31 +0530

    done

--- event-and-exhibition.php
+++ event-and-exhibition.php
@@ -3,11 +3,12 @@
 <section class="py-5 bg-body event-section">
     <div class="container">
         <div class="row row-gap-4">
-            <div class="col-12">
-                <h2 class="display-2 fw-bold text-start text-gradient text-uppercase mb-4">Events & Exhibitions</h2>
+            <div class="col-12 position-relative z-0">
+                <div class="title back-text text-uppercase " style="left:30px;">Events & Exhibitions</div>
+                <h1 class="title text-gradient text-uppercase mb-5 ">Events & Exhibitions</h1>
             </div>
             <?php for($i=0; $i<8; $i++) { ?>
-            <div class="col-lg-3">
+            <div class="col-lg-3 col-md-6">
                 <div class="position-relative">
                     <div class="flip-box">
                         <div class="flip-box-front">
@@ -15,7 +16,7 @@
                                 <img src="images/event3.jpg" alt="Event 3" class="img-fluid" />
                             </figure>
                         </div>
-                        <div class="flip-box-back text-center">
+                        <div class="flip-box-back p-3 text-center">
                             <h5 class="text-white">Nepal Medical Show 2023 Kathmandu Nepal</h5>
                             <p class="text-white"><strong>2,3,4 March 2023</strong></p>
                             <p class="text-white">Stall No. A5 / Hall 1</p>
